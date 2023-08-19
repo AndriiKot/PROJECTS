f41685d68ce8a5ba366cc9ede79234e5d568dd45
AndriiKot <a_kocyuba@ukr.net>
2023-08-19 09:13:48 +0100

    create Lesson-1

---
+++ Tutorials/Tutorials from Timur Shemsedinov/lesson_1/task1/demosetInterval.rb
@@ -0,0 +1,14 @@
+﻿
+MAX_VALUE = 10
+
+def event
+    MAX_VALUE.times do |i|  
+         puts "counter: #{i}, date: #{Time.now}"
+         sleep 0.5
+    end
+end
+
+puts 'Begin'
+event() 
+puts "The End"
+
