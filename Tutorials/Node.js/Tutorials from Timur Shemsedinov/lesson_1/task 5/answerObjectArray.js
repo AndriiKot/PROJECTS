64ccb52056e24f8cbed2a29faa9192d437715614
AndriiKot <a_kocyuba@ukr.net>
2023-08-19 15:48:01 +0100

    color console text

---
+++ Tutorials/Node.js/Tutorials from Timur Shemsedinov/lesson_1/task 5/answerObjectArray.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+
+const welcome = person => {
+    console.log(`Ave, ${person.name}!`);
+};
+
+const persons = [
+    { name: 'Marcus Aurelius' },
+    { name: 'Mao Zedong' },
+    { name: 'Rene Descartes' },
+];
+
+for (const person of persons){
+    welcome(person);
+}
+
