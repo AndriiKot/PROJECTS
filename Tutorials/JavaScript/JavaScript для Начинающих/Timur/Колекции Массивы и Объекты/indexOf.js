675bba535032b81a7cb75be5a565c0e011612fc7
AndriiKot <a_kocyuba@ukr.net>
2023-08-27 21:41:45 +0100

    add Timur lection

---
+++ Tutorials/JavaScript/JavaScript для Начинающих/Timur/Колекции Массивы и Объекты/indexOf.js
@@ -0,0 +1,9 @@
+﻿
+const prices = [10, 1500, 100, 50];
+
+const names = ['Laptop', 'Keyboard', 'Mouse'];
+
+console.log(names.join('; '));
+
+const index = names.indexOf('Mouse');
+console.log({ index });
