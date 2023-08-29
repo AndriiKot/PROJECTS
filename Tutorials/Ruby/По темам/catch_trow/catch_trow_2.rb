7b97211e36705c106652be72c44626ef3c11d56c
AndriiKot <a_kocyuba@ukr.net>
2023-08-29 14:12:53 +0100

    add Ruby rescue

---
+++ Tutorials/Ruby/По темам/catch_trow/catch_trow_2.rb
@@ -0,0 +1,30 @@
+﻿@hh = {}
+
+def add_person name,age
+    @hh[name] = age
+end
+
+def shown_hash
+    @hh.keys.each do |key|
+       age = @hh[key]
+       puts "Name #{key}, age #{age}"
+    end
+end
+
+loop do
+begin
+   print 'Enter yuor name: '
+   name = gets.strip.capitalize
+   break if name == ''
+   print 'Enter yuor age: '
+   age = gets.to_i
+	if age == 0
+   break
+	end
+   age = age.to_i
+
+  add_person name,age
+end
+end
+
+shown_hash
