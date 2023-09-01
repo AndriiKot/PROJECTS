6136563d8eca109ef7baef31ce69fe3ee25ff7c8
AndriiKot <a_kocyuba@ukr.net>
2023-09-02 00:25:58 +0100

    add Функциональное програмирование

---
+++ Tutorials/Ruby/По темам/ФУНКЦИОНАЛЬНОЕ ПРАГРАММИРОВАНИЕ/class Method/two_class_Method.rb
@@ -0,0 +1,25 @@
+﻿
+class Thing
+    def square(n) = n * n
+    def plus(n) = n + n
+end
+
+meth1 = Thing.new.method(:square)
+meth2 = Thing.new.method('plus')
+
+array = *1..10
+
+# p array.collect(&meth2)
+# p array.collect(&Thing.new.method('square'))
+
+array.collect do |i|
+    p meth1[meth2[i]]
+end
+
+array.collect do |i|
+  p (meth1 << meth2)[i]
+end
+
+p array
+
+#array.collect(&meth2).each(&method(:puts))
