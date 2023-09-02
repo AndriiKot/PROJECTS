07f26df1a33ef53b29fc5372a1df93bb81795a58
AndriiKot <a_kocyuba@ukr.net>
2023-09-02 08:16:14 +0100

    testing lambda and proc

---
+++ Tutorials/Ruby/По темам/ФУНКЦИОНАЛЬНОЕ ПРАГРАММИРОВАНИЕ/callBack_Raw/Bm/call_Back_raw.rb
@@ -0,0 +1,21 @@
+﻿require "benchmark"
+
+plus = proc { |a| a + a }
+sque = proc { |a| a * a }
+
+n = 1_000_000_0
+
+Benchmark.bmbm do |x| 
+  x.report(' >> ') do
+    n.times do |i|
+      (plus >> sque)[i]
+    end
+  end 
+
+  x.report(' << ') do
+    n.times do |i| 
+      (sque << plus)[i]
+    end
+  end 
+
+end
---
+++ Tutorials/Ruby/По темам/ФУНКЦИОНАЛЬНОЕ ПРАГРАММИРОВАНИЕ/callBack_Raw/Bm/call_Back_raw_lambda.rb
@@ -0,0 +1,36 @@
+﻿require "benchmark"
+
+plus = proc { |a| a + a }
+sque = proc { |a| a * a }
+
+plusL = lambda { |a| a + a }
+squeL = lambda { |a| a * a }
+
+
+n = 1_000_000
+
+Benchmark.bmbm do |x| 
+  x.report(' >> proc ') do
+    n.times do |i|
+      (plus >> sque)[i]
+    end
+  end 
+
+  x.report(' << proc ') do
+    n.times do |i| 
+      (sque << plus)[i]
+    end
+  end 
+  x.report(' >> lambda ') do
+    n.times do |i|
+      (plusL >> squeL)[i]
+    end
+  end 
+
+  x.report(' << lambda') do
+    n.times do |i| 
+      (squeL << plusL)[i]
+    end
+  end 
+
+end
