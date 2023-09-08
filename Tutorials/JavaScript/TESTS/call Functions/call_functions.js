e4f12839182ecf944a3d6da1b657ead94e23c8f4
AndriiKot <a_kocyuba@ukr.net>
2023-09-08 17:52:16 +0100

    TEST call_function in JS

---
+++ Tutorials/JavaScript/TESTS/call Functions/call_functions.js
@@ -0,0 +1,20 @@
+﻿
+
+const fn1 = (a = 0, b = 0) => {};
+const fn2 = (...args) => { args[0] = args[0] || 0; args[1] = args[1] || 0 }   // ??? Работает очень медленно и в браузере и в Node.js
+
+let n = 1_000_000_000;
+
+const test = (fn,n) => {
+  console.dir(fn);
+
+  console.time();
+
+    for(let i = 0; i < n; i++) { fn() }
+
+  console.timeEnd();  
+};
+
+test(fn1,n)
+test(fn2,n)
+
