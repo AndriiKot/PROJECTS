6f048d922d9582576d7e0a642dc516a325058f2d
AndriiKot <a_kocyuba@ukr.net>
2023-09-08 21:42:45 +0100

    testing function JS

---
+++ Tutorials/JavaScript/TESTS/Functions/functions.js
@@ -0,0 +1,23 @@
+﻿
+
+const fn1 = (a = 0, b = 0) => {};
+function fn2(a = 0, b = 0) {};
+function fn3(a = 0, b = 0){}
+
+let n = 1_000_000_000;
+
+const test = (fn,n) => {
+  console.dir(fn);
+
+  console.time();
+
+    for(let i = 0; i < n; i++) { fn() }
+
+  console.timeEnd();  
+};
+
+test(fn1,n)
+test(fn2,n)
+test(fn3,n)
+
+
