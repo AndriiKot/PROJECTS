68e739449d260461895f63cfe5583ed1bda33d88
AndriiKot <a_kocyuba@ukr.net>
2023-09-10 09:31:15 +0100

    trening ruby and js

---
+++ Tutorials/Ruby/TESTS/Строки/объеденение строк/test1_only_strings.rb
@@ -0,0 +1,58 @@
+﻿
+require 'benchmark'
+                 
+n = 1_000_000_0
+
+Benchmark.bm do |x|
+
+  x.report("не явное склеивание"){ 
+  n.times do
+	 "Hello" "," " " "Oxy!";
+	 "Hi"","" " "Andrii!";
+	 'Lubchik'','' ''the best cat''!';
+  end
+  }
+
+  x.report("+") {
+  n.times do
+	 "Hello" + "," + " " + "Oxy!";
+	 "Hi" + "," + " " + "Andrii!";
+	 'Lubchik'+ ',' + ' ' + 'the best cat''!';
+  end
+  }
+
+  x.report('<<') {
+  n.times do
+	 "Hello" << "," << " " << "Oxy!";
+	 "Hi" << "," << " " << "Andrii!";
+	 'Lubchik' << ',' << ' ' << 'the best cat''!';
+  end
+  }
+
+  x.report('.concat') {
+  n.times do
+	 "Hello".concat ",".concat " ".concat "Oxy!";
+	 "Hi".concat ",".concat " ".concat "Andrii!";
+	 'Lubchik'.concat  ','.concat ' '.concat 'the best cat''!';
+  end
+  }
+
+  x.report('#{}'){ 
+  n.times do
+	 "#{"Hello"}#{","}#{" "}#{'Oxy!'}";
+	 "#{"Hi"}#{","}#{" "}#{"Andrii!"}";
+	 "#{'Lubchik'}#{','}#{' '}#{'the best cat'}#{'!'}";
+  end
+  }
+
+  x.report('%s %'){ 
+  n.times do
+	 "Hello%s" % ",%s" % " %s" % "Oxy!";
+	 "Hi%s" % ",%s" % " %s" % "Andrii!";
+	 'Lubchik%s' % ',%s' % ' %s' % 'the best cat%s' % '!';	
+  end
+  }
+
+end
+
+print 'Push Enter to exit: '; gets
---
+++ Tutorials/Trening/Ruby/2023-09-10/trening.rb
@@ -0,0 +1,8 @@
+﻿
+system "echo 'Hello, Matz!'"
+system "echo", "Hello,", "Oxi!"
+
+puts "Hey Matz, I`m running " + `ruby -v`
+
+ARGV.each {|name| puts "Hello, #{name}!!!" }
+
