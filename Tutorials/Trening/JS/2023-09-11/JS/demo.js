dcf801c5f1dc48fd1a541246bbcfcdf7fc49183c
AndriiKot <a_kocyuba@ukr.net>
2023-09-11 07:41:46 +0100

    lesson 1

---
+++ Tutorials/Trening/JS/2023-09-11/JS/demo.js
@@ -0,0 +1,18 @@
+﻿const values = {
+	name: 'Mouse',
+	price: 25,
+	stock: false,
+	avg: (a, b) => (a + b) / 2n,
+	reference: null,
+	primitive: undefined,
+	numbers: [NaN, Infinity],
+}
+
+for(const item in values) {
+  console.log(`${item}: ${values[item]}`)
+}
+
+for(const item in values) {
+  console.log(`${typeof(values[item])}: ${values[item]}`)
+}
+
