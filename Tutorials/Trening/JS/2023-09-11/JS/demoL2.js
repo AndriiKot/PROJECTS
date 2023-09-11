44e65b70294c774766e457b4b31585b27e822bf4
AndriiKot <a_kocyuba@ukr.net>
2023-09-11 08:17:22 +0100

    lesson-1 lesson-2 lesson-3

---
+++ Tutorials/Trening/JS/2023-09-11/JS/demoL2.js
@@ -0,0 +1,7 @@
+﻿
+const fn1 = (a, b) => { console.log(a + b) }
+
+console.time()
+fn1(3,5)
+console.timeEnd()
+
---
+++ Tutorials/Trening/JS/2023-09-11/JS/demoL3.js
@@ -0,0 +1,4 @@
+﻿
+const array = [111, 222, 333, 444]
+
+for(const item of array) { console.log(item) }
