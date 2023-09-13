9fcfbcdeb64620587de5bad0fb029b33f12fcd8d
AndriiKot <a_kocyuba@ukr.net>
2023-09-13 20:05:29 +0100

    create Decorator

---
+++ Tutorials/JavaScript/JavaScript по темам/Call Bind Apply/декораторы.js
@@ -0,0 +1,21 @@
+﻿
+function printValue(x){
+  // code ...
+  return x * 2;
+};
+
+const cachingDecorator = (fn) => {
+  const cache = new Map();
+
+  return function(x) {
+    if(cache.has(x)) { return cache.get(x) }
+    const result = fn(x);
+    cache.set(x, result)
+
+    return result;
+  };
+}
+
+printValue  = cachingDecorator(printValue);
+
+console.log(printValue(2));
