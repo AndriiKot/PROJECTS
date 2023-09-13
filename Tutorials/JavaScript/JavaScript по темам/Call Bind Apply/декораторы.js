a50e1049b3cd5fe2028cbb773651bccd81da173e
AndriiKot <a_kocyuba@ukr.net>
2023-09-13 20:21:15 +0100

    testing Decorator

--- Tutorials/JavaScript/JavaScript по темам/Call Bind Apply/декораторы.js
+++ Tutorials/JavaScript/JavaScript по темам/Call Bind Apply/декораторы.js
@@ -8,9 +8,13 @@ const cachingDecorator = (fn) => {
   const cache = new Map();
 
   return function(x) {
-    if(cache.has(x)) { return cache.get(x) }
+    if(cache.has(x)) { 
+      console.log('testing cache')
+      return cache.get(x) 
+    }
     const result = fn(x);
     cache.set(x, result)
+    console.log('testing not cache')
 
     return result;
   };
@@ -18,4 +22,5 @@ const cachingDecorator = (fn) => {
 
 printValue  = cachingDecorator(printValue);
 
+console.log(printValue(2));
 console.log(printValue(2));
