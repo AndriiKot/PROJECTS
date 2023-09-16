e3b9b4e81ac3d925884d9a7ae9c6bec7046db9eb
AndriiKot <a_kocyuba@ukr.net>
2023-09-16 22:43:33 +0100

    passing variable to block Ractor

--- Tutorials/Trening/Ruby/2023-09-16/ractor.rb
+++ Tutorials/Trening/Ruby/2023-09-16/ractor.rb
@@ -1,7 +1,10 @@
 ﻿
-ract1 = Ractor.new name: 'main' do 
-  puts "Hello, I am Ractot main!"
-  var1 = 23
+var1 = 40
+
+ract1 = Ractor.new var1,name: 'main' do |var|
+  puts "Hello, I am Ractor main!"
+  var2 = 23 + var
 end
 
 puts ract1.name
+puts ract1.take
