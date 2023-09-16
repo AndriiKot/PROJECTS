dbe40fde91861aaafbe969bddca7131108d772f2
AndriiKot <a_kocyuba@ukr.net>
2023-09-16 22:58:51 +0100

    create Ractor 2

--- Tutorials/Trening/Ruby/2023-09-16/ractor.rb
+++ Tutorials/Trening/Ruby/2023-09-16/ractor.rb
@@ -6,5 +6,17 @@ ract1 = Ractor.new var1,name: 'main' do |var|
   var2 = 23 + var
 end
 
+ract2 = Ractor.new name: 'ractor-2' do 
+  var2 = Ractor.receive
+  var2 = var2 + 12
+end
+
+puts "Ractor: main"
 puts ract1.name
 puts ract1.take
+puts '=' * 44,"\n"
+
+puts "Ractor: ractor-2"
+puts ract2.send var1
+puts ract2.take   
+
