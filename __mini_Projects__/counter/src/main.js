ee5a5cadfc0bf54b8969e425911eb1f1f11e728e
AndriiKot <a_kocyuba@ukr.net>
2024-04-20 23:31:29 +0200

    'counter v.1.0.1'

--- __mini_Projects__/counter/src/main.js
+++ __mini_Projects__/counter/src/main.js
@@ -4,6 +4,7 @@ import CounterComponent from './components/CounterComponent.vue'
 import 'normalize.css'
 import './css/main.css'
 
+
 const app = createApp(App);
 app.component('counter-component', CounterComponent);
 app.mount('#app')
