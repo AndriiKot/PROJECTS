81a018366aa5fb4d0c02dabe86e35cf6791706e7
AndriiKot <a_kocyuba@ukr.net>
2024-04-20 23:08:11 +0200

    'counter' added styles to App

--- __mini_Projects__/counter/src/App.vue
+++ __mini_Projects__/counter/src/App.vue
@@ -10,5 +10,14 @@
   --color-2: #e63946;
   --color-3: #556581;
   --color-4: #11150f;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  min-width: 100vw;
+  text-align: center;
+  overflow: hidden;
+
 }
 </style>
