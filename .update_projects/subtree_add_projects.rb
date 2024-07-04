c35bd7abd5d16edb328c803bc7e234509c599a24
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 23:34:44 +0200

    PROJECTS !!! DELETE PROJECTS  6 !!!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -1,11 +1,11 @@
 ﻿require_relative 'config'
 
-pull = "git subtree add --prefix "
+add = "git subtree add --prefix "
 
 Dir.chdir("..")
 
 PROJECTS.each do |folder, git_repo| 
-    system(`#{pull}#{folder} #{git_repo} main`)
+    system(`#{add}#{folder} #{git_repo} main`)
 end
 
 system(`git push`)
