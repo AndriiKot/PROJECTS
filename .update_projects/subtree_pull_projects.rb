911b21bfb5d4fcd7479d2e09c86ce8e6a8aa87ae
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 20:35:08 +0200

    PROJECTS !!! Parallel gem testing 2 !!!

--- .update_projects/subtree_pull_projects.rb
+++ .update_projects/subtree_pull_projects.rb
@@ -1,21 +1,11 @@
 ﻿require_relative 'config'
-
-# pull = "git subtree pull --prefix "
-
-# PROJECTS.each do |folder, git_repo| 
-#     system(`cd .. && #{pull}#{folder} #{git_repo} main`)
-# end
-
-system(`git push`)
-
-
 require 'parallel'
 
 Parallel.each(PROJECTS, in_processes: PROJECTS.size) do |folder, git_repo|
-  Dir.chdir('..') do
-    system("git subtree pull --prefix #{folder} #{git_repo} main")
-  end
+    Dir.chdir('..') do
+      system("git subtree pull --prefix #{folder} #{git_repo} main")
+    end
 end
-
+    
 system('git push')
 
