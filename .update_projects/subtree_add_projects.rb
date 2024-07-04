bc9944fecd912b7d3c63560e9ba055019be5d017
AndriiKot <a_kocyuba@ukr.net>
2024-07-04 19:15:15 +0200

    PROJECTS !!! update './update_projects' testing 2 !!!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -3,7 +3,7 @@
 add  = "git subtree add --prefix "
 
 PROJECTS.each do |folder, git_repo| 
-        system(`cd .. && #{add}#{folder} #{git_repo} main`) if Dir.exist?("../#{folder}")
+        (system(`cd .. && #{add}#{folder} #{git_repo} main`)) if Dir.exist?("../#{folder}")
 end
 
 system(`git push`)
