e1b4ed0e739c1de8c7682d71f05abdf987bce4e8
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 14:42:39 +0200

    PROJECTS !!! TESTING  not-async script  8 Thread.new   !!!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -7,10 +7,12 @@ Dir.chdir("..")
 PROJECTS.each do |folder, git_repo| 
     if  Dir.exist?(folder)
       puts "\n\n\n#{folder}: Exist !!!!\n\n\n"
-  else 
+    else 
 #    system(`#{add}#{folder} #{git_repo} main`)
 #    exec(`#{add}#{folder} #{git_repo} main`)
+Thread.new do
      spawn(`#{add}#{folder} #{git_repo} main`)
+end
   end
 end
 
