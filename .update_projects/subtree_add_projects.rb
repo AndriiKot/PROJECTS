0cb9b600dafb1ad23d4a69b80a2475cbe902b48f
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 15:39:52 +0200

    PROJECTS !!! TESTING  not-async script  23  !!!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -6,15 +6,10 @@ Dir.chdir("..")
 
 PROJECTS.each do |folder, git_repo| 
    if Dir.exist?(folder)
-     puts "\n #{folder}: Exist !!!\n" 
+     p "\n #{folder}: Exist !!!\n" 
    else
-     sleep 1
      system(`#{add}#{folder} #{git_repo} main`)
    end
 end
 
-if system(`git push`)
-  exit true
-else
-  exit false
-end
+system(`git push`)
--- .update_projects/update.rb
+++ .update_projects/update.rb
@@ -1,6 +1,9 @@
 ﻿
-system(`ruby subtree_add_projects.rb`) 
-sleep 5
-system(`ruby subtree_pull_projects.rb`)
+def m1 = (system(`ruby subtree_add_projects.rb`) )
+
+def m2 = (system(`ruby subtree_pull_projects.rb`))
+
+m1()
+
 #system('ruby subtree_pull_projects-async.rb')  // !!! пропускает проекты не все изменения проходят
 # когда скрип асихронный, это скрипт не отрабатывает все файлы 
