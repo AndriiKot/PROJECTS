3af24349355856374e1f4cf88fc3ac83dfb4f38c
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 13:56:04 +0200

    PROJECTS !!! TESTING  not-async script  6-6 !!!

--- .update_projects/subtree_add_projects.rb
+++ .update_projects/subtree_add_projects.rb
@@ -3,15 +3,14 @@
 add = "git subtree add --prefix "
 
 Dir.chdir("..")
-folder = "Vue-1"
+#folder = "Vue"   ### !!! такая папка на уровень выше есть !!!
 
-#PROJECTS.each do |folder, git_repo|
+PROJECTS.each do |folder, git_repo|
  
     unless Dir.exist?(folder)
     	system(`#{add}#{folder} #{'Vue'} main`)
-    else
-        puts "#{folder}: Exist !!!!" ### !!! Не выводится в терминал !!!
     end
-#end
+        puts "#{folder}: Exist !!!!" ### !!! выводится в терминал !!!
+end
 
 system(`git push`)
