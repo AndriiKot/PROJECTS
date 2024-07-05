a1b5597a95eccba223c49dee8827db90058adf28
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 15:23:49 +0200

    PROJECTS !!! TESTING  not-async script  21-7 system() && system() !!!

--- .update_projects/update.rb
+++ .update_projects/update.rb
@@ -1,4 +1,6 @@
 ﻿
-system(`ruby subtree_add_projects.rb`) && system(`ruby subtree_pull_projects.rb`)
+system(`ruby subtree_add_projects.rb`) 
+sleep 5
+system(`ruby subtree_pull_projects.rb`)
 #system('ruby subtree_pull_projects-async.rb')  // !!! пропускает проекты не все изменения проходят
 # когда скрип асихронный, это скрипт не отрабатывает все файлы 
