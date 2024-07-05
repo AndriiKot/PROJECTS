14f0d8c3afb3de58943ffda4874b958fe49a0e74
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 20:26:48 +0200

    PROJECTS !!! OOP TESTING testing 1 !!!

---
+++ .update_projects/subtree_commnds-2.rb
@@ -0,0 +1,31 @@
+﻿require_relative 'config'
+
+class SubtreeManager
+
+  def initialize(base_path: '../', **project_hash)
+    @projects = project_hash
+    @commands = %w[add pull]
+    @path = base_path
+    @folders = Dir.entries(@path)
+  end
+
+  
+  def manage_subtree()
+    @commands.each do |com|
+      @projects.each do |folder, git_repo|
+        next if (com = 'add' && folders.include?(folder))
+        subtree(com, folder, git_repo)
+      end
+    end
+  end
+
+  private
+  def perform_subtree_command(command, folder_name, git_repository)
+    system("git subtree #{command} --prefix #{folder_name} #{git_repository} main")
+  end
+end
+
+update = SubtreeManager.new(**PROJECTS)
+
+update.manage_subtree
+
