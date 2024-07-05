89e255f1ad485c23f9441177645d3f207d85bb1a
AndriiKot <a_kocyuba@ukr.net>
2024-07-05 21:01:35 +0200

    PROJECTS !!! OOP TESTING testing 8!!!

--- .update_projects/subtree_commands.rb
+++ .update_projects/subtree_commands.rb
@@ -10,31 +10,19 @@ class SubtreeManager
   end
 
   
-  def manage_subtree()
-    # puts "Starting subtree management..."
-    @commands.each do |com|
-      # puts "Performing #{com} command..."
-      # gets
-      @projects.each do |folder, git_repo|
-        # puts "Checking #{folder}..."
-        # gets
-        # next if (com == 'add' && @folders.include?(folder))
-        # puts "Executing command for #{folder}..."
-        # gets
-        next if (com == 'add' && @folders.include?(folder))
-        perform_subtree_command(com, folder, git_repo)
-        # puts "Command for #{folder} executed successfully."
-        # gets
+  def manage_subtree
+    @commands.each do |command|
+      @projects.each do |folder_name, git_repository|
+        next if (command == 'add' && @folders.include?(folder_name))
+        perform_subtree_command(command, folder_name, git_repository)
       end
     end
-    # puts "Subtree management complete."
-    # gets
   end
 
   private
-  def perform_subtree_command(command, folder_name, git_repository)
-    puts "!!!! SYSTEM !!!!"
-    system(`cd #{@path} && git subtree #{command} --prefix #{folder_name} #{git_repository} main`)
+  def perform_subtree_command(action, project_directory, repository_url)
+    subtree_command = "git subtree #{action} --prefix #{project_directory} #{repository_url} main"
+    system("cd #{@path} && #{subtree_command}")
   end
 end
 
