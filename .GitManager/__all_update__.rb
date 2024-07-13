0dd3a7b0769c345e011b125837113b8c576e6a51
AndriiKot <a_kocyuba@ukr.net>
2024-07-13 12:53:44 +0200

    PROJECTS !!! CREATE '__all_update__.rb' testing 5-2 !!!

--- .GitManager/__all_update__.rb
+++ .GitManager/__all_update__.rb
@@ -5,7 +5,7 @@ require_relative './readmeManager_v_1_0_0/update_README.rb'
 main_path = '../'
 
 SubtreeManager.new(path: main_path, **PROJECTS).manage_subtree
-# READMEManager.new(path: main_path, template: "# My Projects: \n\n", config: PROJECTS).update
+READMEManager.new(path: main_path, template: "# My Projects: \n\n", config: PROJECTS).update
 
 
 # pp SubtreeManager.new(path: main_path, **PROJECTS)
