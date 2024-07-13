55fd06e146965319b0710f1583429d8d98f1cf16
AndriiKot <a_kocyuba@ukr.net>
2024-07-13 13:46:24 +0200

    PROJECTS !!! CREATE NEW FUNCTIONAL  .update_README.rg debug  3 !!!

--- .GitManager/readmeManager_v_1_0_0/update_README.rb
+++ .GitManager/readmeManager_v_1_0_0/update_README.rb
@@ -31,7 +31,7 @@ class READMEManager
                        end.count
     end
 
-    def total_count_projects()
+    def total_count_projects
         total_count = 0
             @config.each do |key, value|
               total_count +=  count_projects(key)
@@ -39,7 +39,7 @@ class READMEManager
         total_count
     end
 
-    def create_template() 
+    def create_template
         @new_template += "### Total projects: #{total_count_projects()}\n\n"
         @config.each do |key, value|
           @new_template += "### [#{key}](#{value}): #{count_projects(key)}\n"
