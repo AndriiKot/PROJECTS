63b40f22074178ddc84b3bc93fa70a62a1135322
AndriiKot <a_kocyuba@ukr.net>
2024-07-13 13:31:05 +0200

    PROJECTS !!! rename folders and files !!!

--- .GitManager/__all_update__.rb
+++ .GitManager/__all_update__.rb
@@ -1,5 +1,5 @@
 ﻿require_relative './config'
-require_relative './subtreeManager_v_02_OPP/subtree_OPP.rb'
+require_relative './subtreeManager_v_02_OOP/subtree_OOP.rb'
 require_relative './readmeManager_v_1_0_0/update_README.rb'
 
 main_path = '../'
