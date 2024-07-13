5a4f32000812b341381ce439557b3bb052b729a6
AndriiKot <a_kocyuba@ukr.net>
2024-07-13 12:45:38 +0200

    PROJECTS !!! CREATE '__all_update__.rb' testing 2 !!!

--- .GitManager/__all_update__.rb
+++ .GitManager/__all_update__.rb
@@ -1,7 +1,8 @@
 ﻿require_relative './config'
 require_relative './subtreeManager_v_02_OPP/subtree_OPP.rb'
 
+main_path = '../'
 
-SubtreeManager.new(**PROJECTS).manage_subtree
-READMEManager.new(path: '../', template: "# My Projects: \n\n", config: PROJECTS).update
+SubtreeManager.new(path: main_path, **PROJECTS).manage_subtree
+READMEManager.new(path: main_path, template: "# My Projects: \n\n", config: PROJECTS).update
 
