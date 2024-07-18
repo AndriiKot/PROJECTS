5911fcefbc74c0320e42688c8e5e9144725fdfab
AndriiKot <a_kocyuba@ukr.net>
2024-07-18 09:36:49 +0200

    CSS !! update update_all_projects.rb !!

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -3,6 +3,7 @@
  Text_Animation_RotateY_360deg: 'https://github.com/AndriiKot/Text_RotateY_360deg_.git',
  Images_Grid: 'https://github.com/AndriiKot/Images_Grid.git',
  "Flex-box__01": 'https://github.com/AndriiKot/Flex-box__01.git',
+ "Flex-box__02__Menu__": "https://github.com/AndriiKot/Flex-box__02__Menu__.git",
 }
 
 update = "git subtree pull --prefix "
