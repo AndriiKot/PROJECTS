d4994872bf7e31b34816a016a0bc6c10287a46b5
AndriiKot <a_kocyuba@ukr.net>
2024-07-18 10:11:21 +0200

    CSS !! update update_all_projects.rb !!

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -2,7 +2,7 @@
  Loading: 'https://github.com/AndriiKot/Loading.git',
  Text_Animation_RotateY_360deg: 'https://github.com/AndriiKot/Text_RotateY_360deg_.git',
  Images_Grid: 'https://github.com/AndriiKot/Images_Grid.git',
- "Flex-box__01__Share_of_Facebook__": 'https://github.com/AndriiKot/Flex-box__01.git',
+ "Flex-box__01__Share_of_Facebook__": 'https://github.com/AndriiKot/Flex-box__01__Share_of_Facebook__',
  "Flex-box__02__Menu__": "https://github.com/AndriiKot/Flex-box__02__Menu__.git",
 }
 
