7b4fba3488abd47b7dde8014963eee2b6328b901
AndriiKot <a_kocyuba@ukr.net>
2024-07-25 18:22:21 +0200

    PROJECTS !!! update config.rb !!!

--- .GitManager/config.rb
+++ .GitManager/config.rb
@@ -2,6 +2,6 @@
     "Vue" => 'https://github.com/AndriiKot/Vue.git', 
     "React" => 'https://github.com/AndriiKot/React.git',
     "VanillaJS" => 'https://github.com/AndriiKot/VanillaJS.git', 
-    "CSS" =>'https://github.com/AndriiKot/HTML_and_CSS.git',
+    "HTML_and_CSS" =>'https://github.com/AndriiKot/HTML_and_CSS.git',
 }
 
